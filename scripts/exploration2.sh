6193d565b259693552bc23eeb1ad4fda57406920
Jaewoopudding <ijnn258@gmail.com>
2025-01-11 21:24:47 +0900

    update scripts

--- scripts/exploration2.sh
+++ scripts/exploration2.sh
@@ -1,10 +1,10 @@
 distribution="elliptic_paraboloid"
 
 # KL Divergence Coefficient 리스트
-beta=(0.001 0.002 0.005 0.01 0.02 )
+beta=(0.05 0.1 0.2 0.5 1.0)
 exploration=('non_differentiable_pseudo_count') # 'differentiable_last_pseudo_count' 
-normalization=('standard' 'minmax') 
-device=cuda:2
+normalization=('standard' 'minmax')
+device=cuda:3
 
 # 각 coefficient에 대해 train_ppo.py 실행
 for beta in "${beta[@]}"
